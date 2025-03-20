884bda70d3e03f9871429f4136f506cbe2fb0f60
Murali Das Mohan <muralidm@outlook.com>
2025-03-20 15:12:53 +0530

    Policy Deploy failure - handle delete failures

--- internal/provider/policy_resource_test.go
+++ internal/provider/policy_resource_test.go
@@ -11,6 +11,7 @@ package provider
 
 import (
 	"fmt"
+	"strings"
 	"terraform-provider-ndfc/internal/provider/ndfc"
 	"terraform-provider-ndfc/internal/provider/resources/resource_policy"
 	helper "terraform-provider-ndfc/internal/provider/testing"
@@ -318,4 +319,551 @@ func TestAccPolicyResourceVrfLiteRouted(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(PolicyModelHelperStateCheck("ndfc_policy.policy_test", *policyResource, path.Empty())...),
 			},
 		}})
+}
+
+/*
+	resource "ndfc_policy" "alias" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for alias"
+	  template_name        = "switch_freeform"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    "CONF" = <<-EOT
+	    cli alias name wr copy run start
+	    EOT
+	  }
+	}
+
+	resource "ndfc_policy" "banner" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for banner"
+	  template_name        = "banner"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    BANNER = "VXLAN as Code Banner"
+	    BANNERDELIMITER = "_"
+	    TYPE = "motd"
+	  }
+	}
+
+	resource "ndfc_policy" "SSH_KEY_2048" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for SSH_KEY_2048"
+	  template_name        = "ssh_key_rsa_force"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    BITS = 2048
+	  }
+	}
+
+	resource "ndfc_policy" "iBGP_ISN1_ISN3" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for iBGP_ISN1_ISN3"
+	  template_name        = "vrf_lite_ibgp"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    vrfName = "default"
+	    BGP_ASN = "29500"
+	    NEIGHBOR_IP = "100.65.0.3"
+	    bgpPassword = "9125d59c18a9b015"
+	    bgpPasswordKeyType = 3
+	  }
+	}
+
+	resource "ndfc_policy" "iBGP_ISN1_ISN2" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for iBGP_ISN1_ISN2"
+	  template_name        = "vrf_lite_ibgp"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    vrfName = "default"
+	    BGP_ASN = "29500"
+	    NEIGHBOR_IP = "100.65.0.5"
+	    bgpPassword = "9125d59c18a9b015"
+	    bgpPasswordKeyType = 3
+	  }
+	}
+
+	resource "ndfc_policy" "iBGP_ISN1_ISN4" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for iBGP_ISN1_ISN4"
+	  template_name        = "vrf_lite_ibgp"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    vrfName = "default"
+	    BGP_ASN = "29500"
+	    NEIGHBOR_IP = "100.65.0.9"
+	    bgpPassword = "9125d59c18a9b015"
+	    bgpPasswordKeyType = 3
+	  }
+	}
+
+	resource "ndfc_policy" "iBGP_ISN2_ISN1" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for iBGP_ISN2_ISN1"
+	  template_name        = "vrf_lite_ibgp"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    vrfName = "default"
+	    BGP_ASN = "29500"
+	    NEIGHBOR_IP = "100.65.0.4"
+	    bgpPassword = "9125d59c18a9b015"
+	    bgpPasswordKeyType = 3
+	  }
+	}
+
+	resource "ndfc_policy" "iBGP_ISN2_ISN3" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for iBGP_ISN2_ISN3"
+	  template_name        = "vrf_lite_ibgp"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    vrfName = "default"
+	    BGP_ASN = "29500"
+	    NEIGHBOR_IP = "100.65.0.1"
+	    bgpPassword = "9125d59c18a9b015"
+	    bgpPasswordKeyType = 3
+	  }
+	}
+
+	resource "ndfc_policy" "iBGP_ISN2_ISN4" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for iBGP_ISN2_ISN4"
+	  template_name        = "vrf_lite_ibgp"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    vrfName = "default"
+	    BGP_ASN = "29500"
+	    NEIGHBOR_IP = "100.65.0.7"
+	    bgpPassword = "9125d59c18a9b015"
+	    bgpPasswordKeyType = 3
+	  }
+	}
+
+	resource "ndfc_policy" "iBGP_ISN3_ISN1" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for iBGP_ISN3_ISN1"
+	  template_name        = "vrf_lite_ibgp"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    vrfName = "default"
+	    BGP_ASN = "29500"
+	    NEIGHBOR_IP = "100.65.0.2"
+	    bgpPassword = "9125d59c18a9b015"
+	    bgpPasswordKeyType = 3
+	  }
+	}
+
+	resource "ndfc_policy" "iBGP_ISN3_ISN2" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for iBGP_ISN3_ISN2"
+	  template_name        = "vrf_lite_ibgp"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    vrfName = "default"
+	    BGP_ASN = "29500"
+	    NEIGHBOR_IP = "100.65.0.0"
+	    bgpPassword = "9125d59c18a9b015"
+	    bgpPasswordKeyType = 3
+	  }
+	}
+
+	resource "ndfc_policy" "iBGP_ISN3_ISN4" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for iBGP_ISN3_ISN4"
+	  template_name        = "vrf_lite_ibgp"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    vrfName = "default"
+	    BGP_ASN = "29500"
+	    NEIGHBOR_IP = "100.65.0.10"
+	    bgpPassword = "9125d59c18a9b015"
+	    bgpPasswordKeyType = 3
+	  }
+	}
+
+	resource "ndfc_policy" "eBGP_ISN3_MPLS_PE" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for eBGP_ISN3_MPLS_PE"
+	  template_name        = "External_VRF_Lite_eBGP"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    vrfName = "default"
+	    vrf_description = "To MPLS-PE1"
+	    asn = "29500"
+	    NEIGHBOR_ASN = 65111
+	    NEIGHBOR_IP = "100.65.0.13"
+	    bgpPassword = "9125d59c18a9b015"
+	    bgpPasswordKeyType = 3
+	  }
+	}
+
+	resource "ndfc_policy" "iBGP_ISN4_ISN1" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for iBGP_ISN4_ISN1"
+	  template_name        = "vrf_lite_ibgp"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    vrfName = "default"
+	    BGP_ASN = "29500"
+	    NEIGHBOR_IP = "100.65.0.6"
+	    bgpPassword = "9125d59c18a9b015"
+	    bgpPasswordKeyType = 3
+	  }
+	}
+
+	resource "ndfc_policy" "iBGP_ISN4_ISN2" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for iBGP_ISN4_ISN2"
+	  template_name        = "vrf_lite_ibgp"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    vrfName = "default"
+	    BGP_ASN = "29500"
+	    NEIGHBOR_IP = "100.65.0.8"
+	    bgpPassword = "9125d59c18a9b015"
+	    bgpPasswordKeyType = 3
+	  }
+	}
+
+	resource "ndfc_policy" "iBGP_ISN4_ISN3" {
+	  is_policy_group      = false
+	  deploy               = true
+	  entity_name          = "Switch"
+	  entity_type          = "SWITCH"
+	  description          = "Policy for iBGP_ISN4_ISN3"
+	  template_name        = "vrf_lite_ibgp"
+	  source               = "CLI"
+	  priority             = 500
+	  device_serial_number = "9Q34PHYLDB5"
+	  policy_parameters = {
+	    vrfName = "default"
+	    BGP_ASN = "29500"
+	    NEIGHBOR_IP = "100.65.0.11"
+	    bgpPassword = "9125d59c18a9b015"
+	    bgpPasswordKeyType = 3
+	  }
+	}
+*/
+func TestAccPolicyResourceMultiple(t *testing.T) {
+
+	x := &map[string]string{
+		"RscType":  ndfc.ResourcePolicy,
+		"RscName":  "",
+		"User":     helper.GetConfig("policy").NDFC.User,
+		"Password": helper.GetConfig("policy").NDFC.Password,
+		"Host":     helper.GetConfig("policy").NDFC.URL,
+		"Insecure": helper.GetConfig("policy").NDFC.Insecure,
+	}
+
+	tf_config := new(string)
+	*tf_config = `provider "ndfc" {
+        host     = "https://"
+        username = "admin"
+        password = "admin!@#"
+        domain   = "example.com"
+        insecure = true
+        }
+        resource ndfc_vrfs "net_test" {
+            fabric_name = "dummy"
+        }`
+
+	stepCount := new(int)
+	*stepCount = 0
+
+	// Create a new instance of the NDFC client
+	policyResources := []*resource_policy.NDFCPolicyModel{
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for alias",
+			TemplateName:       "switch_freeform",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"CONF": "cli alias name wr copy run start"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for banner",
+			TemplateName:       "banner",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"BANNER": "VXLAN as Code Banner", "BANNERDELIMITER": "_", "TYPE": "motd"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for iBGP_ISN1_ISN3",
+			TemplateName:       "vrf_lite_ibgp",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"vrfName": "default", "BGP_ASN": "29500", "NEIGHBOR_IP": "100.65.0.3", "bgpPassword": "9125d59c18a9b015", "bgpPasswordKeyType": "3"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for iBGP_ISN1_ISN2",
+			TemplateName:       "vrf_lite_ibgp",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"vrfName": "default", "BGP_ASN": "29500", "NEIGHBOR_IP": "100.65.0.5", "bgpPassword": "9125d59c18a9b015", "bgpPasswordKeyType": "3"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for iBGP_ISN1_ISN4",
+			TemplateName:       "vrf_lite_ibgp",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"vrfName": "default", "BGP_ASN": "29500", "NEIGHBOR_IP": "100.65.0.9", "bgpPassword": "9125d59c18a9b015", "bgpPasswordKeyType": "3"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for iBGP_ISN2_ISN3",
+			TemplateName:       "vrf_lite_ibgp",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"vrfName": "default", "BGP_ASN": "29500", "NEIGHBOR_IP": "100.65.0.1", "bgpPassword": "9125d59c18a9b015", "bgpPasswordKeyType": "3"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for iBGP_ISN2_ISN1",
+			TemplateName:       "vrf_lite_ibgp",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"vrfName": "default", "BGP_ASN": "29500", "NEIGHBOR_IP": "100.65.0.4", "bgpPassword": "9125d59c18a9b015", "bgpPasswordKeyType": "3"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for iBGP_ISN2_ISN4",
+			TemplateName:       "vrf_lite_ibgp",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"vrfName": "default", "BGP_ASN": "29500", "NEIGHBOR_IP": "100.65.0.7", "bgpPassword": "9125d59c18a9b015", "bgpPasswordKeyType": "3"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for iBGP_ISN3_ISN1",
+			TemplateName:       "vrf_lite_ibgp",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"vrfName": "default", "BGP_ASN": "29500", "NEIGHBOR_IP": "100.65.0.2", "bgpPassword": "9125d59c18a9b015", "bgpPasswordKeyType": "3"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for iBGP_ISN3_ISN2",
+			TemplateName:       "vrf_lite_ibgp",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"vrfName": "default", "BGP_ASN": "29500", "NEIGHBOR_IP": "100.65.0.0", "bgpPassword": "9125d59c18a9b015", "bgpPasswordKeyType": "3"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for iBGP_ISN3_ISN4",
+			TemplateName:       "vrf_lite_ibgp",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"vrfName": "default", "BGP_ASN": "29500", "NEIGHBOR_IP": "100.65.0.10", "bgpPassword": "9125d59c18a9b015", "bgpPasswordKeyType": "3"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for eBGP_ISN3_MPLS_PE",
+			TemplateName:       "External_VRF_Lite_eBGP",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"vrfName": "default", "vrf_description": "To MPLS-PE1", "asn": "29500", "NEIGHBOR_ASN": "65111", "NEIGHBOR_IP": "100.65.0.13", "bgpPassword": "9125d59c18a9b015", "bgpPasswordKeyType": "3"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for iBGP_ISN4_ISN1",
+			TemplateName:       "vrf_lite_ibgp",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"vrfName": "default", "BGP_ASN": "29500", "NEIGHBOR_IP": "100.65.0.6", "bgpPassword": "9125d59c18a9b015", "bgpPasswordKeyType": "3"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for iBGP_ISN4_ISN2",
+			TemplateName:       "vrf_lite_ibgp",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"vrfName": "default", "BGP_ASN": "29500", "NEIGHBOR_IP": "100.65.0.8", "bgpPassword": "9125d59c18a9b015", "bgpPasswordKeyType": "3"},
+			Deleted:            new(bool),
+		},
+		{
+			Deploy:             true,
+			EntityName:         "Switch",
+			EntityType:         "SWITCH",
+			Description:        "Policy for iBGP_ISN4_ISN3",
+			TemplateName:       "vrf_lite_ibgp",
+			Source:             "CLI",
+			Priority:           new(int64),
+			DeviceSerialNumber: helper.GetConfig("policy").NDFC.Switches[0],
+			PolicyParameters:   map[string]string{"vrfName": "default", "BGP_ASN": "29500", "NEIGHBOR_IP": "100.65.0.11", "bgpPassword": "9125d59c18a9b015", "bgpPasswordKeyType": "3"},
+			Deleted:            new(bool),
+		},
+	}
+	policyRsArr := []interface{}{}
+	for _, policyResource := range policyResources {
+		*policyResource.Priority = 500
+		if (*x)["RscName"] == "" {
+			(*x)["RscName"] = strings.Split(policyResource.Description, " ")[2]
+		} else {
+			(*x)["RscName"] = (*x)["RscName"] + "," + strings.Split(policyResource.Description, " ")[2]
+		}
+		*policyResource.Deleted = false
+		policyRsArr = append(policyRsArr, policyResource)
+	}
+
+	resourceNames := strings.Split((*x)["RscName"], ",")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t, "policy") },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: func() string {
+					*stepCount++
+					tName := fmt.Sprintf("%s_%d", t.Name(), *stepCount)
+					helper.GetTFConfigWithSingleResource(tName, *x, policyRsArr, &tf_config)
+					return *tf_config
+				}(),
+
+				Check: resource.ComposeTestCheckFunc(
+					func() []resource.TestCheckFunc {
+						ret := []resource.TestCheckFunc{}
+						for i, policyResource := range policyResources {
+							ret = append(ret, PolicyModelHelperStateCheck("ndfc_policy."+resourceNames[i], *policyResource, path.Empty())...)
+						}
+						return ret
+					}()...),
+			},
+		},
+	})
 }
