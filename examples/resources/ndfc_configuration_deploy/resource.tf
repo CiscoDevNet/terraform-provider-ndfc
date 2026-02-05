663ad5a0765f83b17ef343dffd022d4d795fc565
Murali Das Mohan <muralidm@outlook.com>
2026-02-05 12:33:52 +0530

    Support recalculate-only option in config-deploy resource

--- examples/resources/ndfc_configuration_deploy/resource.tf
+++ examples/resources/ndfc_configuration_deploy/resource.tf
@@ -1,7 +1,8 @@
 
 resource "ndfc_configuration_deploy" "test_resource_configuration_deploy_1" {
-  fabric_name              = "CML"
-  serial_numbers           = ["ALL"]
-  config_save              = true
-  trigger_deploy_on_update = false
+  fabric_name    = "CML"
+  serial_numbers = ["ALL"]
+  recalculate    = true
+  always_execute = false
+  deploy         = true
 }
